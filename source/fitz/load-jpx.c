75f6261fc5769470249ec01f40447da69ed6a68b
simon987 <me@simon987.net>
2020-07-19 12:41:31 -0400

    Possible fix for simon987/sist2#83

--- source/fitz/load-jpx.c
+++ source/fitz/load-jpx.c
@@ -534,7 +534,7 @@ static fz_context *get_opj_context(void)
 
 void opj_lock(fz_context *ctx)
 {
-	fz_lock(ctx, FZ_LOCK_FREETYPE);
+	fz_lock(ctx, FZ_LOCK_JPX);
 
 	set_opj_context(ctx);
 }
@@ -543,7 +543,7 @@ void opj_unlock(fz_context *ctx)
 {
 	set_opj_context(NULL);
 
-	fz_unlock(ctx, FZ_LOCK_FREETYPE);
+	fz_unlock(ctx, FZ_LOCK_JPX);
 }
 
 void *opj_malloc(size_t size)
